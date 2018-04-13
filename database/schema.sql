e0415da6732d4f7a64e76a8a505c1b1b507dc693
Laurent Frazier <laurentfrazier@gmail.com>
2018-04-12 19:44:01 -0700

    wrote postgres database schema

---
+++ database/schema.sql
@@ -0,0 +1,19 @@
+DROP DATABASE IF EXISTS "instructions";
+CREATE DATABASE "instructions";
+\connect instructions;
+CREATE TABLE recipes(
+  id SERIAL PRIMARY KEY,
+  info JSON NOT NULL
+);
+
+INSERT INTO recipes (info) VALUES
+  (
+    '{
+      "recipe": {
+        "step": 1,
+        "title": "cheesesteak",
+        "photo":  "https://cook.ba/2INgFln",
+        "instruction": "Preheat the oven to 450°F. Wash and dry the fresh produce. Cut the potatoes lengthwise into 1/4-inch-thick sticks. Cut off and discard the bottom 1/2 inch of the broccoli stem; cut the broccoli into large florets. Combine in a large bowl."
+      }
+    }'
+);
