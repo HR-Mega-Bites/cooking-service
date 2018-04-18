e428df58d2d6f49e9b3c106d5fb5ce9779eb2718
Laurent Frazier <laurentfrazier@gmail.com>
2018-04-17 23:41:00 -0700

    fixed bug in package.json file

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const path = require('path');
 const axios = require('axios');
-const bodparser = require('body-parser');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-const port = 2700;
+app.use(express.static(path.join(__dirname, '../public/dist')));
+
+app.use(bodyParser.json());
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false});
+
+const port = 2900;
 
 app.listen(port, () => console.log('listening on port ', port));
