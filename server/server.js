9ba535f822d232f7732bbf528385d7bc220d40b4
HourglassMagi <laurent.frazier@jetsetsuccess.net>
2018-04-18 13:43:21 -0700

    Update server.js
    fixed merge conflicts

--- server/server.js
+++ server/server.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const path = require('path');
 const axios = require('axios');
-<<<<<<< HEAD
 const bodyParser = require('body-parser');
-=======
-const bodyparser = require('body-parser');
->>>>>>> 8d841ec295253037850d747731b09b6b91165ee9
 
 const app = express();
 
