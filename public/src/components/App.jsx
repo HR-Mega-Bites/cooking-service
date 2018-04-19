bb3f3454e11afb938bb1b823938f228ed63b07d6
HourglassMagi <laurent.frazier@jetsetsuccess.net>
2018-04-19 14:32:46 -0700

    Update App.jsx
    Made App a React Component class
    configured getRecipe method using axios
    created a test button to ensure that the button was making call to server and retrieving expected result from database

--- public/src/components/App.jsx
+++ public/src/components/App.jsx
@@ -1,4 +1,29 @@
 import React from 'react';
-const App = () => (<div>
-  <h1>Instructions</h1>
-</div>)
+import Axios from 'axios';
+
+class App extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      recipe: []
+    };
+    this.getRecipe = this.getRecipe.bind(this)
+  };
+  getRecipe() {
+    Axios.get('http://127.0.0.1:3100/recipe')
+    .then((res) => {
+      console.log('this is the response', res.data);
+    })
+
+  }
+  render() {
+    return (
+      <div>
+        <h1>Instructions</h1>
+        <h2>good boy</h2>
+        <button onClick={this.getRecipe}>click me</button>
+      </div>
+    )
+  }
+}
+export default App;
