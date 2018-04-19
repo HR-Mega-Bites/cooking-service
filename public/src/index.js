fb8dcb132d60a2f7a4d44f474f6e1cfe6cd01f95
Laurent Frazier <laurentfrazier@gmail.com>
2018-04-19 14:44:52 -0700

    code clean up for public files

--- public/src/index.js
+++ public/src/index.js
@@ -2,11 +2,4 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import App from './components/App';
 
-const title = 'My Minimal React Webpack Babel Setup';
-
-ReactDom.render(
-  <div>
-    <App />
-  </div>,
-  document.getElementById('app')
-);
+ReactDom.render(<App />, document.getElementById('app'));
