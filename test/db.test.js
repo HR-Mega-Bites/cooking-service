c5889ce3b6f70172ec5924dad457d2676215b827
Laurent Frazier <laurentfrazier@gmail.com>
2018-04-18 21:07:54 -0700

    jest test passed for db, to unit test written

---
+++ test/db.test.js
@@ -0,0 +1,29 @@
+const db = require('../database/index.js')
+
+describe('Database', () =>{
+
+  it('should retrive all records in the database', (done) => {
+    db.client.query(`SELECT * from recipes`, (err, res) => {
+      if(err) {
+        throw err
+      } else {
+        var results = res.rows.length;
+        console.log(results);
+        expect(results).toBe(101);
+        done();
+      }
+    });
+  });
+  it('should return a single record when given an id', (done) => {
+    db.client.query(`SELECT * from recipes where id = 9`, (err, res) => {
+      if(err) {
+        throw err
+      } else {
+        var record = res.rows[0].id;
+        console.log(record);
+        expect(record).toBe(9);
+        done();
+      }
+    })
+  })
+})
