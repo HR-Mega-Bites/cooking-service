928091f88f531851644ac7b235353e34897b5e61
Laurent Frazier <laurentfrazier@gmail.com>
2018-04-19 16:15:14 -0700

    updated state with get request response data from server call to database

--- public/src/components/App.jsx
+++ public/src/components/App.jsx
@@ -1,4 +1,33 @@
 import React from 'react';
-const App = () => (<div>
-  <h1>Instructions</h1>
-</div>)
+import Axios from 'axios';
+
+class App extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      recipe: []
+    };
+    this.getRecipe = this.getRecipe.bind(this)
+  };
+  getRecipe() {
+    Axios.get('http://127.0.0.1:3300/recipe')
+    .then((res) => {
+      this.setState({
+        recipe: res.data.recipe
+      });
+    })
+    // .then(console.log('resonse is here', response))
+    console.log('i was clicked')
+  }
+  render() {
+    return (
+      <div>
+        <h1>Instructions</h1>
+        <h2>good boy</h2>
+        <button onClick={this.getRecipe}>click me</button>
+
+      </div>
+    )
+  }
+}
+export default App;
