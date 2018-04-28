395128a032550740f96f02e0bdbdcbcc2eb8537d
Laurent Frazier <laurentfrazier@gmail.com>
2018-04-28 15:54:20 -0700

    ;;

--- server/server.js
+++ server/server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const axios = require('axios');
 const bodyParser = require('body-parser');
-const db = require('../database/index.js');
+// const db = require('../database/index.js');
 
 const app = express();
-console.log(db);
+console.log('im alive');
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -18,12 +18,12 @@ app.use(express.static(path.join(__dirname, '../public/dist')));
 app.use(bodyParser.json());
 // const urlencodedParser = bodyParser.urlencoded({ extended: false});
 
-app.get('/recipes/:id/instructions', (req, res) => {
-    db.getRecipe(req.params.id, res);
-
-    // res.send(db.getRecipe(4));
-  // }
-});
+// app.get('/recipes/:id/instructions', (req, res) => {
+//     db.getRecipe(req.params.id, res);
+//
+//     // res.send(db.getRecipe(4));
+//   // }
+// });
 
 const port = 3700;
 
